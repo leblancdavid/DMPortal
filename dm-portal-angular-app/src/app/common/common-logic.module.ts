2d14f3ddf431f9e407a4190bb07e8ac7bb00dba5
David Leblanc <lblancdavid@gmail.com>
2022-02-26 07:07:19 -0800

    Added value icons

--- dm-portal-angular-app/src/app/common/common-logic.module.ts
+++ dm-portal-angular-app/src/app/common/common-logic.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreatureStatComponent } from './creature-stat/creature-stat.component';
 import { MaterialModule } from '../material/material.module';
+import { ValueIconComponent } from './value-icon/value-icon.component';
 
 
 
 @NgModule({
-  declarations: [CreatureStatComponent],
-  exports: [CreatureStatComponent],
+  declarations: [CreatureStatComponent, ValueIconComponent],
+  exports: [CreatureStatComponent, ValueIconComponent],
   imports: [
     CommonModule,
     MaterialModule
