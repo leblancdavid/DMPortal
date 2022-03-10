98cc9595a26b0c4b23608cddbe1be8c857585d7b
David Leblanc <lblancdavid@gmail.com>
2022-03-10 09:53:06 -0800

    Added the edit dialog

--- dm-portal-angular-app/src/app/adventure/player-editor/player-editor.component.ts
+++ dm-portal-angular-app/src/app/adventure/player-editor/player-editor.component.ts
@@ -9,6 +9,7 @@ import { Player } from '../player';
 })
 export class PlayerEditorComponent implements OnInit {
 
+  classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
   constructor(@Inject(MAT_DIALOG_DATA) public player: Player) { }
 
   ngOnInit(): void {
